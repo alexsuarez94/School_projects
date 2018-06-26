0e90e2f4f95bc9fe8f422dfac0d25d38fbf378a5
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-06-26 17:15:14 +0200

    change with url

--- star_wars_scrapping.R
+++ star_wars_scrapping.R
@@ -1,15 +1,18 @@
 # films <- c("http://www.imdb.com/title/tt1490017/", "https://www.imdb.com/title/tt0121765/?ref_=ttls_li_tt",
 #            "https://www.imdb.com/title/tt0121766/?ref_=ttls_li_tt", "https://www.imdb.com/title/tt0076759/?ref_=ttls_li_tt")
 
-# with urls
+# with url
+
+prueba <- read_html("https://www.imdb.com/list/ls070150896/")
+
 url <- prueba %>%
-  html_nodes(".lister-item-header, h3 a") %>%
+  html_nodes(" h3 a") %>%
   html_attr('href') %>%
   na.exclude()
 url <- unlist(url)[1:9]
 url
 
-
+#assign main path to every movie
 for (i in 1:length(url)){
 films <- paste0("http://www.imdb.com", url, sep = "")
 }
