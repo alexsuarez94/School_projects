92dcd88f2f85e3ff4ea418e08aee615149a7ad97
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-06-27 18:00:42 +0200

    twitter

---
+++ twitter.R
@@ -0,0 +1,11 @@
+library(twitteR)
+
+
+twitter_key <- "JooeFz2g7i4VbWAIwpddOL4dr"
+twitter_secret <- "d4FP2t9O8Nxa8lAcoNu8x3YtQpJWE41dJ0B6aeVhQnazEt0j44"
+
+access_token <- "463023825-DtEr0IqLS9WJbTPTxkJnoxcNdMgrmC0Pw8Vig2s9"
+access_secret <- "mr3d9Y9x5IXN38aTRi7mAtbMlECDlQUssaEMPaxWsBMBL"
+
+
+oauth <- setup_twitter_oauth(consumer_key = twitter_key, consumer_secret = twitter_key, access_token = access_token, access_secret= access_secret)
