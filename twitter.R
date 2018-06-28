9a96663242298689f767edd9d6e3714da4c0bd0a
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-06-28 17:32:10 +0200

    key_twitter

--- twitter.R
+++ twitter.R
@@ -14,21 +14,39 @@ oauth <- setup_twitter_oauth(twitter_key,
                               access_secret)
 
 ###
+myTweets <- searchTwitter("#refugees", n = 100)
 
-search.string <- "#Trump"
-no.of.tweets <- 1000
-myTweets <- searchTwitter(search.string, n=no.of.tweets, 
-                        since='2017-05-01', until='2017-05-12', lang="en")
-head(myTweets)
+tweets.text <- sapply(myTweets, function(x) x$getText())
+# Replace @UserName
+ tweets.text <- gsub("@\\w+", "", tweets.text)
+ # Remove punctuation
+tweets.text <- gsub("[[:punct:]]", "", tweets.text)
+# Remove links
+tweets.text <- gsub("http\\w+", "", tweets.text)
+# Remove tabs
+tweets.text <- gsub("[ |\t]{2,}", "", tweets.text)
+# remove codes that are neither characters nor digits
+tweets.text<-  gsub( '[^A-z0-9_]', ' ', tweets.text)
+# Set characters to lowercase
+tweets.text <- tolower(tweets.text)
+# Replace blank space ("rt")
+tweets.text <- gsub("rt", "", tweets.text)
+# Remove blank spaces at the beginning
+tweets.text <- gsub("^ ", "", tweets.text)
+# Remove blank spaces at the end
+tweets.text <- gsub(" $", "", tweets.text)
+head(tweets.text)
 
+require("tm")
+tweets.text.corpus <- Corpus(VectorSource(tweets.text))
+head(stopwords())
 
+tweets.text.corpus <- tm_map(tweets.text.corpus, function(x) removeWords(x, stopwords()))
+head(tweets.text.corpus)
 
+require(wordcloud)
 
-
-
-
-
-
+wordcloud(tweets.text.corpus, min.freq = 2, scale= c(7,0.5), colors=brewer.pal(8, "Dark2"),random.color= TRUE, random.order = FALSE, max.words = 150)
 
 
 
