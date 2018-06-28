1665f4dac246f48939be8147746c941707906887
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-06-28 15:56:10 +0200

    key_twitter

--- twitter.R
+++ twitter.R
@@ -4,8 +4,52 @@ library(twitteR)
 twitter_key <- "JooeFz2g7i4VbWAIwpddOL4dr"
 twitter_secret <- "d4FP2t9O8Nxa8lAcoNu8x3YtQpJWE41dJ0B6aeVhQnazEt0j44"
 
-access_token <- "463023825-DtEr0IqLS9WJbTPTxkJnoxcNdMgrmC0Pw8Vig2s9"
-access_secret <- "mr3d9Y9x5IXN38aTRi7mAtbMlECDlQUssaEMPaxWsBMBL"
+access_token <- "463023825-aTRXAw2V3WYXjWhMCOpOHEWrTe2yVOGGvdwyI0E8"
+access_secret <- "Yy5FnjsYD34jCablVxY1AviescYo1ryD843lJrTGytP4t"
 
 
-oauth <- setup_twitter_oauth(consumer_key = twitter_key, consumer_secret = twitter_key, access_token = access_token, access_secret= access_secret)
+oauth <- setup_twitter_oauth(twitter_key,
+                             twitter_secret, 
+                             access_token,
+                              access_secret)
+
+###
+
+search.string <- "#Trump"
+no.of.tweets <- 1000
+myTweets <- searchTwitter(search.string, n=no.of.tweets, 
+                        since='2017-05-01', until='2017-05-12', lang="en")
+head(myTweets)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
