9eed40041c5c2e87fbd7317214e39b6c07a13d95
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-06-28 20:09:06 +0200

    changes
    adding reaction to player, season and chart type

--- nba/server.R
+++ nba/server.R
@@ -12,7 +12,12 @@
 # http://www.gregreda.com/2015/02/15/web-scraping-finding-the-api/
 
 library(shiny)
-
+library(shinydashboard)
+library(ggplot2)
+library(RCurl)
+library(grid)
+library(jpeg)
+library(hexbin)
 
 
 # Define server logic required to draw a histogram
@@ -28,24 +33,106 @@ shinyServer(function(input, output) {
       1,
       2
     )
-  
-    df.player <- shotDataf[[player]][[1]]
     
-    resp <- list(df.player)
+    season <- switch(
+      as.integer(input$season),
+      1,
+      2, 
+      3, 
+      4
+    )
+    
+    chart <- switch(
+      as.integer(input$chart), 
+      "a",
+      "b", 
+      "c"
+    )
+  
+    
+    df.player <- shotDataf[[player]][[season]]
+    
+    resp <- list(df.player, chart)
     resp
   })
 
   
-  output$shotChart <- renderPlot({
-    
-    # generate plot from input$playerID from ui.R
-    x <- as.integer(input$playerID)
-    data <- nba_reactive()[[1]]
-    
-    #ggplot of made vs missed shots (without court for the moment)
-    ggplot(data, aes(x=LOC_X, y=LOC_Y)) +
-      geom_point(aes(colour = EVENT_TYPE))
   
-  })
+
+    output$shotChart <- renderPlot({
+
+      if(nba_reactive()[[2]] == "a"){
+
+
+      #generate plot from input$playerID from ui.R
+      data <- nba_reactive()[[1]]
+
+    #ggplot of made vs missed shots (without court for the moment)
+      ggplot(data, aes(x=LOC_X, y=LOC_Y)) +
+        geom_point(aes(colour = EVENT_TYPE))
+
+      } else if(nba_reactive()[[2]] == "b"){
+      
+       # half court image
+        data <- nba_reactive()[[1]]
+        courtImg.URL <- "https://thedatagame.files.wordpress.com/2016/03/nba_court.jpg"
+        court <- rasterGrob(readJPEG(getURLContent(courtImg.URL)),
+                   width=unit(1,"npc"), height=unit(1,"npc"))
+    
+        # plot using NBA court background and colour by shot zone
+        ggplot(data, aes(x=LOC_X, y=LOC_Y)) +
+          annotation_custom(court, -250, 250, -50, 420) +
+          geom_point(aes(colour = SHOT_ZONE_BASIC, shape = EVENT_TYPE)) +
+          xlim(250, -250) +
+          ylim(-50, 420) +
+          # geom_rug(alpha = 0.2) +
+          # coord_fixed() +
+          ggtitle(paste("Shot Chart\n", unique(data$PLAYER_NAME), sep = "")) +
+          theme(line = element_blank(),
+                axis.title.x = element_blank(),
+                axis.title.y = element_blank(),
+                axis.text.x = element_blank(),
+                axis.text.y = element_blank(),
+                legend.title = element_blank(),
+                plot.title = element_text(size = 15, lineheight = 0.9, face = "bold"))
+        
+
+
+        } else if (nba_reactive()[[2]] == "c"){
+
+          data <- nba_reactive()[[1]]
+          courtImg.URL <- "https://thedatagame.files.wordpress.com/2016/03/nba_court.jpg"
+          court <- rasterGrob(readJPEG(getURLContent(courtImg.URL)),
+                              width=unit(1,"npc"), height=unit(1,"npc"))
+
+          # plot shots using ggplot, hex bins, NBA court backgroung image.
+          ggplot(data, aes(x=LOC_X, y=LOC_Y)) + 
+            annotation_custom(court, -250, 250, -52, 418) +
+            stat_binhex(bins = 25, colour = "gray", alpha = 0.7) +
+            scale_fill_gradientn(colours = c("yellow","orange","red")) +
+            guides(alpha = FALSE, size = FALSE) +
+            xlim(250, -250) +
+            ylim(-52, 418) +
+            #for resizing
+            # geom_rug(alpha = 0.2) +
+            # coord_fixed() +
+            ggtitle(paste("Shot Chart\n", unique(data$PLAYER_NAME), sep = "")) +
+            theme(line = element_blank(),
+                  axis.title.x = element_blank(),
+                  axis.title.y = element_blank(),
+                  axis.text.x = element_blank(),
+                  axis.text.y = element_blank(),
+                  legend.title = element_blank(),
+                  plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))
+          
+          
+          }#close else if statement
+     })
+    
+  
+    
+    
+    
+    
   
 })
--- nba/ui.R
+++ nba/ui.R
@@ -1,37 +1,46 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
+library(RCurl)
+library(grid)
+library(jpeg)
+library(hexbin)
 
 source("nba_shot_stats.R")
 
-# Define UI for application that draws a histogram
-shinyUI(
-  fluidPage(
-    
-    # Application title
-    titlePanel("NBA"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
 
-      sidebarPanel(
-        selectInput("playerID", "Select a player:", choices = list("Stephen Curry" = 1, "Lebron James"  = 2), selected = 1)
-
-      ),
-
-      # Show a plot of the generated distribution
-      mainPanel(
-        plotOutput("shotChart")
-      )
-    )
+dashboardPage(
+  
+  dashboardHeader(title = "NBA shot analytics"),
+  
+  #Sidebar content
+  dashboardSidebar(
+    selectInput("playerID", "Select a player:", choices = list("Stephen Curry" = 1, "Lebron James" = 2), selected = 2),
+    selectInput("season", "Select a season", choices = list("2014-15" = 1, "2015-16" = 2, "2016-17" = 3, "2017-18" = 4), selected = 4),
+    selectInput("chart", "Select shotChart", choices = list("a" = 1, "b" = 2, "c" = 3), selected = 1)
     
-  )
-)
+  ), #close dashboard sidebar
+  
+  dashboardBody(
+    
+    fluidRow(
+      box(status = "primary", plotOutput("shotChart"), width = 8),
+      box(status = "primary", title = "info player", width = 4, 
+          "Each player will have here a photo a some basic pshicical measures")
+      
+    ), #close fluid Row
+    
+    
+    fluidRow(
+      
+      box(status = "primary", title = "basic statistics", width = 8, 
+          
+        "Here somo basic statistics of the season will be displayed")
+      
+    ) #close fluidRow
+    
+  ) #close dashboardbody
+  
+  
+) #close dashboardPage
