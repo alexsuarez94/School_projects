758e7e5cca75145fa8bd0ab0d560d3a61bb1f9ce
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-07-05 15:44:48 +0200

    final project

---
+++ Suarez_Otero_Alejandro_Final_project/data/lebron_stats.R
@@ -0,0 +1,36 @@
+# shot data forlebron
+source("player_id.R")
+playerID <- player_id[1]
+season <- c("2014-15", "2015-16", "2016-17", "2017-18")
+
+####################
+basic_stat <- rep( list(rep(list(list()), length(season))), length(playerID) ) 
+
+for (i in 1:length(playerID)){
+  for (j in 1:length(season)){
+    
+    basic_stat[[i]][[j]] <- rjson::fromJSON(file = paste0("https://stats.nba.com/stats/playerdashboardbyyearoveryear?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerID=", playerID[i], "&PlusMinus=N&Rank=N&Season=", season[j],"&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&Split=yoy&VsConference=&VsDivision=", sep = ""), method = "C")
+    
+  }
+}
+
+###################
+
+basic_Stats <- rep( list(rep(list(list()), length(season))), length(playerID) )
+# unlist shot data, save into a data frame
+for (i in 1:length(playerID)){
+  for (j in 1:length(season)){
+    
+    basic_Stats[[i]][[j]] <- data.frame(matrix(unlist(basic_stat[[i]][[j]]$resultSets[[1]][[3]]), ncol=65, byrow = TRUE))
+    # shot data headers
+    colnames(basic_Stats[[i]][[j]]) <- basic_stat[[i]][[j]]$resultSets[[1]][[2]]
+    
+  }
+}
+
+#to create dataframe for each player. 
+df_lbj <- ldply(basic_Stats[[1]], data.frame)[ , c(2, 4, 6, 7, 8,22, 23, 24, 25, 26, 29, 31)]
+
+save(df_lbj, file ="lebron_stats.RData")
+
+
---
+++ Suarez_Otero_Alejandro_Final_project/data/nba_shot_stats.R
@@ -0,0 +1,56 @@
+######################################################################################
+
+# if (!require(rjson)) install.packages("rjson")
+
+######################################################################################
+source("player_id.R") 
+
+# shot data for Stephen Curry
+playerID <- player_id
+season <- c("2014-15", "2015-16", "2016-17", "2017-18")
+
+
+# import from JSON
+
+shotData <- rep( list(rep(list(list()), length(season))), length(playerID) ) 
+
+for (i in 1:length(playerID)){
+  for (j in 1:length(season)){
+    
+shotData[[i]][[j]] <- rjson::fromJSON(file = paste0("http://stats.nba.com/stats/shotchartdetail?Period=0&VsConference=&LeagueID=
+00&LastNGames=0&TeamID=0&Position=&Location=&Outcome=&ContextMeasure=FGA&DateFrom=&StartPeriod=
+&DateTo=&OpponentTeamID=0&ContextFilter=&RangeType=&Season=",season[j],"&AheadBehind=&PlayerID=", playerID[i],"&EndRange=
+&VsDivision=&PointDiff=&RookieYear=&GameSegment=&Month=0&ClutchTime=&StartRange=&EndPeriod=&SeasonType=Regular
++Season&SeasonSegment=&GameID=&PlayerPosition=", sep = ""), method="C")
+
+  }
+}
+
+
+shotDataf <- rep( list(rep(list(list()), length(season))), length(playerID) )
+# unlist shot data, save into a data frame
+for (i in 1:length(playerID)){
+  for (j in 1:length(season)){
+    
+    shotDataf[[i]][[j]] <- data.frame(matrix(unlist(shotData[[i]][[j]]$resultSets[[1]][[3]]), ncol=24, byrow = TRUE))
+    # shot data headers
+    colnames(shotDataf[[i]][[j]]) <- shotData[[i]][[j]]$resultSets[[1]][[2]]
+    
+    # covert x and y coordinates into numeric
+    shotDataf[[i]][[j]]$LOC_X <- as.numeric(as.character(shotDataf[[i]][[j]]$LOC_X))
+    shotDataf[[i]][[j]]$LOC_Y <- as.numeric(as.character(shotDataf[[i]][[j]]$LOC_Y))
+    shotDataf[[i]][[j]]$SHOT_DISTANCE <- as.numeric(as.character(shotDataf[[i]][[j]]$SHOT_DISTANCE))
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
