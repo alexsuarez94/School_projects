68f226182874549079f4b1fdbee6a1b31ac729d1
Suarez, Alejandro <alejandro.suarez@accenture.com>
2018-07-06 09:24:54 +0200

    final project

--- app.R
+++ app.R
@@ -15,7 +15,7 @@
 #library(rvest)
 
 list.of.packages <- c("ggplot2", "shiny", "shinydashboard", "RCurl", "grid", "jpeg", 
-                      "hexbin", "plyr", "shinyjs", "gridExtra", "rjson", "jsonlite")
+                      "hexbin", "plyr", "shinyjs", "gridExtra", "rjson", "jsonlite", "rvest", "dplyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
